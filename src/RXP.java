eff65b62434960ea408a74bd85a9cecf75cc230c
Tyler <tsmith328@gatech.edu>
2015-12-12 16:25:01 -0500

    Added description.

--- src/RXP.java
+++ src/RXP.java
@@ -1,5 +1,7 @@
 /**
- * Created by Tyler on 12/12/2015.
+ * Allows for the reliable transfer of data between two endpoints.
+ * This particular instance is built on top of UDP.
+ * @author Tyler Smith
  */
 public class RXP {
 }
